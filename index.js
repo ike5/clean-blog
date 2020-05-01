57977a6c05ac0a7908e08e79a0140869f6319b96
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-04-30 18:48:21 -0700

    Added public folder to move all static files inside

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
 const app = new express()
-
+app.use(express.static('public'))
 
 const PORT = 4000
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
