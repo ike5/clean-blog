77ac5eda6c7a13f187492eb90f7abbb7f23214ba
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-05-04 13:02:14 -0700

    Changed app.get to app.post!

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = new express()
+const express = require('express') 
+const app = new express() 
 const ejs = require('ejs')
 const fileUpload = require('express-fileupload')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const PORT = 4000
+const validateMiddleware = require('./middleware/validationMiddleware')
+const newPostController = require('./controllers/newPost')
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
 
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 
@@ -15,17 +20,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload())
 
-const newPostController = require('./controllers/newPost')
-const homeController = require('./controllers/home')
-const storePostController = require('./controllers/storePost')
-const getPostController = require('./controllers/getPost')
-const validateMiddleware = require('./middleware/validationMiddleware')
-
 app.use('/posts/store', validateMiddleware)
 
-app.get('/post/:id', getPostController)
-app.get('/', homeController)
-app.get('/posts/store', storePostController)
 app.get('/posts/new', newPostController)
+app.get('/', homeController)
+app.get('/post/:id', getPostController)
+
+app.post('/posts/store', storePostController)
 
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) })
